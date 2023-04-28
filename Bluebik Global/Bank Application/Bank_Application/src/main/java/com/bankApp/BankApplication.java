1d2ec62affb3b3add9fe068955770ee427b038bb
11-DEEPAK-KUMAR-11 <3511.deepak@gmail.com>
2023-04-28 12:08:23 +0530

    Bank Application completed

--- Bluebik Global/Bank Application/Bank_Application/src/main/java/com/bankApp/BankApplication.java
+++ Bluebik Global/Bank Application/Bank_Application/src/main/java/com/bankApp/BankApplication.java
@@ -3,7 +3,10 @@ package com.bankApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class BankApplication {
 
 	public static void main(String[] args) {
